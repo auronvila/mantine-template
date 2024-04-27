532bf21944298715c40cad60aae8a6890a533be6
auronvila <auronvila1121@gmail.com>
2024-04-27 22:55:37 +0300

    update route types

--- src/@types/routes.tsx
+++ src/@types/routes.tsx
@@ -1,19 +1,8 @@
-import type { LazyExoticComponent, ReactNode } from 'react'
-
-export interface Meta {
-  pageContainerType?: 'default' | 'gutterless' | 'contained'
-  header?: string | ReactNode
-  headerContainer?: boolean
-  extraHeader?: LazyExoticComponent<() => JSX.Element>
-  footer?: boolean
-}
 
 export type Route = {
   key: string
   path: string
-  component: LazyExoticComponent<<T extends Meta>(props: T) => JSX.Element>
   authority: string[]
-  meta?: Meta
 }
 
 export type Routes = Route[]
