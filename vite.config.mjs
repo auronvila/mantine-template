f5f95baa18aa95ceb5285dc6f95be888b68d4637
auronvila <auronvila1121@gmail.com>
2025-02-20 13:12:57 +0300

    fix: path

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,7 +4,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  base:'mantine-template',
+  base:'/mantine-template/',
   test: {
     globals: true,
     environment: 'jsdom',
