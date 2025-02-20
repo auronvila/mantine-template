c55d2db344f17d45ca11c760d0df7de7c3743a2d
auronvila <auronvila1121@gmail.com>
2025-02-20 13:07:07 +0300

    fix: update workflow to deploy from dist folder and set a base url for vite

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,6 +4,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  base:'mantine-template',
   test: {
     globals: true,
     environment: 'jsdom',
