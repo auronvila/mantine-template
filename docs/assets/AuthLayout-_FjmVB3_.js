c5ddf36b86b2bcdfb851396ccebb7d7fc0786438
auronvila <auronvila1121@gmail.com>
2025-02-20 13:01:51 +0300

    feat: add docs folder for github pages

---
+++ docs/assets/AuthLayout-_FjmVB3_.js
@@ -0,0 +1,1 @@
+import{k as t}from "./index-y8OWP3zI.js";import{V as r}from "./Views-WvrQ7LwQ.js";function e(){return t.jsx(t.Fragment,{children:t.jsx(r,{})})}export{e as default};
---
+++ docs/assets/Manage-R8YcHzPl.js
@@ -0,0 +1,1 @@
+import{k as a}from "./index-y8OWP3zI.js";function n(){return a.jsx(a.Fragment,{children:"Manage"})}export{n as default};
